ae7242a9ace937bf86059778dd20fccc43cc71c7
e513131 <emma.fu5@gmail.com>
2021-11-08 08:33:10 -0800

    Update Detection.py

--- Detection.py
+++ Detection.py
@@ -7,14 +7,14 @@ porcupine = None
 pa = None
 audio_stream = None
 
-ser = serial.Serial('\\.\COM3', 9600)
-# ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('\\.\COM3', 9600) # change COM port here if necessary
+# ser = serial.Serial('/dev/ttyACM0', 9600) # use this one if running on raspi
 
 ser.flush()
 
 try: 
-    porcupine = pvporcupine.create(keyword_paths=['trick-or-treat__en_windows_2021-11-17-utc_v1_9_0.ppn'])
-    # porcupine = pvporcupine.create(keyword_paths=['trick-or-treat__en_raspberry-pi_2021-11-17-utc_v1_9_0.ppn'])
+    porcupine = pvporcupine.create(keyword_paths=['trick-or-treat__en_windows_2021-11-17-utc_v1_9_0.ppn']) # use this one for windows testing
+    # porcupine = pvporcupine.create(keyword_paths=['trick-or-treat__en_raspberry-pi_2021-11-17-utc_v1_9_0.ppn']) # use this one if on raspi
 
     pa = pyaudio.PyAudio()
     audio_stream = pa.open(
